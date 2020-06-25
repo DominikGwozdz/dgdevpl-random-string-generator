89faa444de2fe3dfdfdb4a8c346192c2afce7d1f
Dominik Gwóźdź <dwgwozdz@gmail.com>
2020-06-25 08:04:26 +0200

    initialize commit

---
+++ src/dgdevplgenerator/Generator.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace dgdevplgenerator;
+
+class RandomStringGenerator
+{
+    public static function generate($length) : string
+    {
+        $finalString = null;
+        $availableChars = [
+            'A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '_'
+        ];
+        for ( $i = 0; $i<$length; $i++) {
+            $finalString .= $availableChars[rand(0, count($availableChars)-1)];
+        }
+        return $finalString;
+    }
+}
